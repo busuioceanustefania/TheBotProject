c439d70f8cf7974252173ab59a9054ce37c59be4
PROJECT <busuioceanu.stefania@gmail.com>
2022-11-30 00:20:01 +0200

    drop table vs1

--- OOP PROIECT/Source.cpp
+++ OOP PROIECT/Source.cpp
@@ -1,6 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <string>
+using namespace std;
+
+class DROP_TABLE {
+
+public:
+	 string tableName=" ";
+
+	DROP_TABLE()   {
+		this->setTableName("studenti");
+
+	}
+
+	 void setTableName(string tableName) {
+		 this->tableName = tableName;
+	 }
+
+	 string getTableName() {
+		 return this->tableName;
+	 }
+
+};
 
 int main() {
-	std::cout << std::endl << "Hello world!";
+	
+	string a= "";
+	DROP_TABLE tabel= DROP_TABLE();
+	while (true) {
+		cout << endl << "input: ";
+		getline(cin, a);
+		/*if (a != "") {
+
+		}*/
+
+	}
 
 }
